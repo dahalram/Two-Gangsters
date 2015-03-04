2edf854fc7177de5646b20a6fc82bc99a3ea9a76
unknown <ramhari.dahal.23@gmail.com>
2015-03-04 14:43:59 -0500

    Added Comments

--- TwoGangsters.cpp
+++ TwoGangsters.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main(){
 	int a, b;
+
+	//Take two integers from the input
 	cin >> a;
 	cin >> b;
 
+	//Print the number that each player misses
 	cout << b-1 << " ";
 	cout << a-1;
 }
